4ce1ab873dc8908339bfcfd955773b1785112dda
Rodrigo <a20202021@pucp.edu.pe>
2023-04-15 17:37:11 -0500

    refactor: :art:

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import Header from './components/Header/Header.js';
+import Formulario from './components/Formulario/Formulario.js';
+
 function App() {
   return (
     <div>
       <Header />
+      <Formulario />
     </div>
   );
 }
